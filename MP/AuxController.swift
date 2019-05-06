bcd080462e9c36ba774f93ba6f20927f147f562b
Tony Aiello <tonyaiello@Tonys-Mac.local>
2019-05-06 13:37:18 -0700

    Bugfixes - duplicate resources, reduce warnings to 1

--- MP/AuxController.swift
+++ MP/AuxController.swift
@@ -5,22 +5,22 @@
 //  Created by Tony Aiello on 5/4/19.
 //  Copyright © 2019 Tony Aiello. All rights reserved.
 //
-
-import Foundation
-import YoutubePlayer_in_WKWebView
-
-class YouTubeViewController: WKWebView {
+//
+//import Foundation
+//import YoutubePlayer_in_WKWebView
+//
+//class YouTubeViewController: WKWebView {
     // This controller is not fully written
     // but it should use the helper kit listed below
     // to help embed youtube videos in a view
     
-    
-    @IBOutlet weak var youtubebox: WKYTPlayerView!
-    
-    func setVID (vid: String) {
-       
-        
-    }
-    
-    
-}
+//
+//    @IBOutlet weak var youtubebox: WKYTPlayerView!
+//
+//    func setVID (vid: String) {
+//
+//
+//    }
+//
+//
+//}
