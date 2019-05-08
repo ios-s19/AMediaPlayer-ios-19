7eaf51d3b27fa5133ef03f9b56a6db391744d40e
Anthony Aiello <aiell016@rangers.uwp.edu>
2019-05-07 18:49:10 -0700

    Added documentation in app delegate comment section

--- MP/AppDelegate.swift
+++ MP/AppDelegate.swift
@@ -4,22 +4,49 @@
 //  Copyright © 2019 Tony Aiello. All rights reserved.
 //
 
-// I researched several tutorials, some very old, some new, some with Youtube
-// The base one I learned a lot from started with this tutorial
+// I researched a few media player tutorials, some very old, some new, some with Youtube
+// In addition to the concepts we learned in class (i.e. cell reuse, table views, etc)
+// one of the informative utils came from this tutorial
+//
 // raywenderlich.com/5191-video-streaming-tutorial-for-ios-getting-started
+//
+// With that tutorial, I continued to build out the app using the AVFoundation package
+// and the AVPlayer to manage the playback of an AV source.
+// I started with a streaming radio app idea using genres. We did that in the previous android class.
+// I wanted to make this a little more exciting. At first, I wanted album art, then playing a radio
+// stream or mp3 file seemed ridiculously easy with what I was building.  So, I decided to incorporate
+// video and make it more of a challenge.
+//
+//
 
-// other tutorials includes appcoda.com/youtube-api-ios-tutorial/  and
+// I found other helpful information at  appcoda.com/youtube-api-ios-tutorial/
+//
+//    and
 // developer.apple.com/library/archive/documentation/AudioVideo/Conceptual/MediaPlaybackGuide/Contents/Resources/en.lproj/GettingStarted/GettingStarted.html#//apple_ref/doc/uid/TP40016757-CH10-SW2
 
 
 // It should be noted a different viewer is required to embed YouTube videos
 //
-// Instead, I hosted my own remote videos on my server
-// and stream them under an https protocol
+// Instead of streaming from YouTube, I hosted my own remote videos on my server
+// and streamed them under an https protocol inside of the AVPlayer.
+// I was surprised at how the AVFoundation's player managed the stream
+// and it's buffering.  It's a very nice library to use to build a media player.
 //
-// Only the original tutorial travel blog videos are included in the local videos
-// folder.  Clips and artwork for the remote videos are included locally.
+// There is also a helper library for handling YouTube videos. But, I didn't have time to
+// fix the bugs to incorporate that.
 //
+// For this project, I made class variables to hold information about a video or audio object
+// its thumbnail location, it's filename or streaming resource location, and the media type, and such.
+//
+//
+// Ideally, I wanted to store the list of video objects as a JSON in a remote file.  This way, the
+// app could read the list from the remote server, or a cloud or Google Drive file, and display a totally different
+// playlist, controlled remotely with this file.
+//
+// For now, the music playlist and other video information and URLs are stored locally and hard coded in.
+//
+// Clips and artwork for the remote videos are included locally, but this could be pulled from a URL also.
+// 
 // 
 
 
